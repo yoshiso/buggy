a546e8d98f2192d1d5438d206b217cdb57fd4c68
yss44 <nya060@gmail.com>
2013-09-23 22:37:29 +0900

    サーバーサイド都のつなぎ込み完了

--- app/assets/javascripts/app/models/project.js.coffee
+++ app/assets/javascripts/app/models/project.js.coffee
@@ -1,5 +1,5 @@
 class App.Models.Project extends Backbone.Model
-
+    url:"/projects"
 
 
 class App.Collections.Projects extends Backbone.Collection
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require ./lib/csrf_hack
 //= require handlebars.runtime
 //= require ./lib/underscore
 //= require ./lib/backbone
